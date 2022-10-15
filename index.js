23df619f100a15155a191e85964b92dd80b3d3aa
JDeveloperD <jsandovalbd1993@gmail.com>
2022-10-15 01:49:31 -0500

    clear index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.use('/api', (req, res) => { return res.status(200).json({ info: 'api-ts-stater' }); });
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => console.log('ready port'));
